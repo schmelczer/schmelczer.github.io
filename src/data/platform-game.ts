c171dcce9302d5ee92e1543c1798a5ab0f8ad401
schmelczerandras <schmelczer.andras@simonyi.bme.hu>
2020-11-27 12:16:09 +0100

    Add video of the platform game

--- src/data/platform-game.ts
+++ src/data/platform-game.ts
@@ -1,12 +1,17 @@
 import platformJpeg from '../static/media/platform.png?format=jpg';
-import platformWebP from '../static/media/platform.png?format=webp';
+import platformMp4 from '../static/media/mp4/platform.mp4';
+import platformWebM from '../static/media/webm/platform.webm';
 
-import { Image } from '../page/basics/image/image';
+import { Video } from '../page/basics/video/video';
 
 export const platformGameTimelineElement = {
   date: `2017 autumn`,
   title: `Platform game`,
-  figure: new Image(platformWebP, platformJpeg, `a picture of the app`),
+  figure: new Video({
+    poster: platformJpeg,
+    mp4: platformMp4,
+    webm: platformWebM,
+  }),
   description: `A 3D game written in C with the help of SDL 1.2 (I haven't heard of GPU programming at the time).`,
   more: [
     `
