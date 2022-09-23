888cdf2c968d77a5ff92779425750ff4d5a8c919
Andras Schmelczer <andras@schmelczer.dev>
2022-09-23 22:45:51 +0200

    Fix video playback

--- src/page/basics/video/video.ts
+++ src/page/basics/video/video.ts
@@ -10,28 +10,27 @@ export class Video extends PageElement {
     super(createElement(generate(options)));
     this.video = this.query('video') as HTMLVideoElement;
     this.video.addEventListener('click', this.startVideo.bind(this));
+
     this.video.addEventListener('play', () =>
       this.htmlRoot.classList.add('fully-loaded')
     );
     this.query('.start-button').addEventListener('click', this.startVideo.bind(this));
-    this.video.addEventListener('pause', this.stopVideo.bind(this));
+    this.video.addEventListener('pause', this.pauseVideo.bind(this));
   }
 
-  private startVideo(e: Event) {
+  private startVideo() {
     if (this.video.paused) {
       this.query('.start-button').style.visibility = 'hidden';
       this.htmlRoot.classList.add('loaded');
       this.video.play();
       this.video.controls = true;
-      e.preventDefault();
     }
   }
 
-  private stopVideo(e: Event) {
+  private pauseVideo() {
     if (this.video.paused) {
       this.query('.start-button').style.visibility = 'visible';
       this.video.controls = false;
-      e.preventDefault();
     }
   }
 }
